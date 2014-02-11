fc9ec41eff11350148c3b55502d378acd30b016e
xoyip <xoyip@piyox.info>
2014-02-11 11:09:17 +0900

    initial

---
+++ Replication/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Replication::Application.config.secret_key_base = '7a4ba4ac81e730e8e816dae3e0c783e87cb88f36b15d5005850f9bea4a4f68ff59accad26771488eadcc620c26efbdd306c774b4d818fdfc03b624bd4811c687'
---
+++ Replication/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Replication::Application.config.session_store :cookie_store, key: '_Replication_session'
